56897f96bceacd284686f1bd3881c11e059aee64
Michael Bayne <mdb@samskivert.com>
2018-07-03 18:11:18 -0700

    Added graphql-yoga, changed test server to serve it.
    We'll need to figure out how to reinstate our webpack auto-reloading fanciness
    once we have an actual client talking to our very simple new GQL server.

--- test/server.js
+++ test/server.js
@@ -2,14 +2,35 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('../webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: false,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
+const { GraphQLServer } = require('graphql-yoga')
 
-  console.log('Listening at localhost:3000');
-});
+const typeDefs = `
+type Query {
+  description: String
+}
+`
+
+const resolvers = {
+  Query: {
+    description: () => `This is a test of the Emergency GraphQL Broadcast System`
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => console.log(`The server is running on http://localhost:4000`))
+
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   hot: false,
+//   historyApiFallback: true
+// }).listen(3000, 'localhost', function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log('Listening at localhost:3000');
+// });
